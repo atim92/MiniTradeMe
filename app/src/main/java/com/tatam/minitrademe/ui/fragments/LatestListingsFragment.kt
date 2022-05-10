1bf7ebf5557b1d89d48998accb30458307273747
Temir Atambayev <a.temirlan@gmail.com>
2022-05-10 21:25:08 +1200

    Implement list of listings

--- app/src/main/java/com/tatam/minitrademe/ui/fragments/LatestListingsFragment.kt
+++ app/src/main/java/com/tatam/minitrademe/ui/fragments/LatestListingsFragment.kt
@@ -1,23 +1,27 @@
 package com.tatam.minitrademe.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.tatam.minitrademe.R
+import com.tatam.minitrademe.data.network.models.Listing
 import com.tatam.minitrademe.databinding.LatestListingsFragmentBinding
+import com.tatam.minitrademe.ui.adapters.ListingAdapter
 import com.tatam.minitrademe.ui.viewmodels.LatestListingsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LatestListingsFragment : Fragment() {
 
-    private lateinit var binding : LatestListingsFragmentBinding
+    private lateinit var binding: LatestListingsFragmentBinding
+    private val viewModel by viewModels<LatestListingsViewModel>()
 
     companion object {
         fun newInstance() = LatestListingsFragment()
     }
-
-    private lateinit var viewModel: LatestListingsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,11 +31,51 @@ class LatestListingsFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        //Lambda for clicking on listing item in case further action needed
+        val openListingDetails = { listing: Listing ->
+            Toast.makeText(requireContext(), "Listing ID ${listing.listingId} Details Pressed!", Toast.LENGTH_SHORT).show()
+        }
+
+        //Setting up a list
+        val adapter = ListingAdapter(openListingDetails, requireContext())
+        binding.rvLatestListings.adapter = adapter
+
+        //Processing states
+        viewModel.listingStateLiveData.observe(viewLifecycleOwner) { state ->
+            if (state.isLoading) {
+                binding.loading.visibility = View.VISIBLE
+            }
+            if (state.error.isNotBlank()) {
+                binding.textError.text = state.error
+                binding.loading.visibility = View.GONE
+                binding.textError.visibility = View.VISIBLE
+            }
+            if (state.listings.isNotEmpty()) {
+                adapter.setItems(state.listings)
+                binding.loading.visibility = View.GONE
+                binding.rvLatestListings.visibility = View.VISIBLE
+            }
+            binding.swipeToRefreshListings.isRefreshing = false
+        }
+
+        binding.swipeToRefreshListings.setOnRefreshListener {
+            binding.rvLatestListings.removeAllViews()
+            viewModel.getListings()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
     }
 
+    //AppBar menu icons
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.latest_listing_menu, menu)
     }
@@ -49,11 +93,4 @@ class LatestListingsFragment : Fragment() {
             else -> super.onOptionsItemSelected(item)
         }
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LatestListingsViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
