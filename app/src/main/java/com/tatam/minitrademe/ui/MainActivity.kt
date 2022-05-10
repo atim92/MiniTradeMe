400ed88cf7dd36970680c6f395f46ca2d91c625c
Temir Atambayev <a.temirlan@gmail.com>
2022-05-10 21:26:30 +1200

    Fix navigation controller inflation error and listings rv bottom constraint

--- app/src/main/java/com/tatam/minitrademe/ui/MainActivity.kt
+++ app/src/main/java/com/tatam/minitrademe/ui/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.tatam.minitrademe.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.tatam.minitrademe.R
 import com.tatam.minitrademe.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
@@ -18,22 +18,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navController = this.findNavController(R.id.nav_fragment)
-        navController.addOnDestinationChangedListener(object : NavController.OnDestinationChangedListener{
-            override fun onDestinationChanged(
-                controller: NavController,
-                destination: NavDestination,
-                arguments: Bundle?
-            ) {
-                title = when (destination.id) {
-                    R.id.latestListingsFragment -> resources.getString(R.string.bottom_menu_title_latest_listings)
-                    R.id.watchlistFragment -> resources.getString(R.string.bottom_menu_title_watchlist)
-                    R.id.myTradeMeFragment -> resources.getString(R.string.bottom_menu_title_my_trade_me)
-                    else -> resources.getString(R.string.app_name)
-                }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_frag) as NavHostFragment
+        val navController = navHostFragment.navController
+        //Switching titles for bottom nav bar
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            title = when (destination.id) {
+                R.id.latestListingsFragment -> resources.getString(R.string.bottom_menu_title_latest_listings)
+                R.id.watchlistFragment -> resources.getString(R.string.bottom_menu_title_watchlist)
+                R.id.myTradeMeFragment -> resources.getString(R.string.bottom_menu_title_my_trade_me)
+                else -> resources.getString(R.string.app_name)
             }
-
-        })
+        }
         binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
